6a7845b4aced7dccfe3270e98d97e2d1b41938a4
Willow Mauldin <willow@mauldin.me>
2025-06-15 21:16:34 +0000

    collect data from json endpoint instead

--- collect.sh
+++ collect.sh
@@ -4,6 +4,6 @@ set -eou pipefail
 export TZ=America/Los_Angeles
 DESTINATION_DIR=$(dirname "$(realpath $0)")/data/$(date '+%Y-%m-%d')
 mkdir -p $DESTINATION_DIR
-DESTINATION=$DESTINATION_DIR/$(date '+%H%M').proto
+DESTINATION=$DESTINATION_DIR/$(date '+%H%M').json
 
-curl "http://developer.trimet.org/ws/V1/VehiclePositions?appid=$TRIMET_APP_ID" -o $DESTINATION
+curl "https://developer.trimet.org/ws/v2/vehicles?showNonRevenue=true&onRouteOnly=true&appid=$TRIMET_APP_ID" -o $DESTINATION
